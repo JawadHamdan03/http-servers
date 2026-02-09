336692943987ef072ffb23a257a87b5e4a3321dc
JawadHamdan03 <eng.jawad121@hotmail.com>
2026-02-09 17:47:13 +0200

    arch✅

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,10 @@
 import express from "express";
+import { apiConfig } from "./config.js";
+// middlewares
+function middlewareMetricsInc(req, res, next) {
+    apiConfig.fileserverHits++;
+    next();
+}
 const middlewareLogResponses = (req, res, next) => {
     res.on("finish", () => {
         const statusCode = res.statusCode;
@@ -8,16 +14,39 @@ const middlewareLogResponses = (req, res, next) => {
     });
     next();
 };
+// handlers
 const handlerReadiness = async (req, res) => {
     res
         .set("Content-Type", "text/plain; charset=utf-8")
         .send("OK");
 };
+const handlerAdminMetrics = async (req, res) => {
+    res
+        .set("Content-Type", "text/html; charset=utf-8")
+        .send(`
+<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited ${apiConfig.fileserverHits} times!</p>
+  </body>
+</html>
+`);
+};
+const handlerReset = async (req, res) => {
+    apiConfig.fileserverHits = 0;
+    res
+        .set("Content-Type", "text/plain; charset=utf-8")
+        .send("OK");
+};
+////////////////////////////////////////////
 const app = express();
 const PORT = 8080;
+app.use("/app", middlewareMetricsInc);
 app.use(middlewareLogResponses);
 app.use("/app", express.static("./src/app"));
-app.get("/healthz", handlerReadiness);
+app.get("/api/healthz", handlerReadiness);
+app.get("/admin/metrics", handlerAdminMetrics);
+app.get("/admin/reset", handlerReset);
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
 import express, { Request, Response,  NextFunction } from "express";
+import {apiConfig} from "./config.js";
+
+
+// middlewares
+function middlewareMetricsInc(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void {
+  apiConfig.fileserverHits++;
+  next();
+}
 
 
 const middlewareLogResponses = (
@@ -20,22 +32,55 @@ const middlewareLogResponses = (
 };
 
 
-
+// handlers
 const handlerReadiness = async (req: Request, res: Response): Promise<void> => {
   res
     .set("Content-Type", "text/plain; charset=utf-8")
     .send("OK");
 };
 
+const handlerAdminMetrics = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  res
+    .set("Content-Type", "text/html; charset=utf-8")
+    .send(`
+<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited ${apiConfig.fileserverHits} times!</p>
+  </body>
+</html>
+`);
+};
 
+
+const handlerReset = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  apiConfig.fileserverHits = 0;
+
+  res
+    .set("Content-Type", "text/plain; charset=utf-8")
+    .send("OK");
+};
+
+
+
+////////////////////////////////////////////
 const app = express();
 const PORT = 8080;
 
-
+app.use("/app", middlewareMetricsInc);
 app.use(middlewareLogResponses);
 
 app.use("/app",express.static("./src/app"));
-app.get("/healthz", handlerReadiness);
+app.get("/api/healthz", handlerReadiness);
+app.get("/admin/metrics", handlerAdminMetrics);
+app.get("/admin/reset", handlerReset);
+
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
